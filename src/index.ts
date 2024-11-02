7b66c5f1f738c0c006d1914f4c8cfb68f44f0e85
Aman Kumar Sinha <amankr7100@gmail.com>
2024-11-02 13:07:39 +0530

    port changes for deploy

--- src/index.ts
+++ src/index.ts
@@ -3,21 +3,16 @@ import { json } from 'express';
 import routes from './routes/routes';
 import AppDataSource from './data-source';
 import {config} from 'dotenv';
-import { error } from 'console';
-import { User } from './entity/user';
-import { Room } from './entity/room';
-import { getRepository } from 'typeorm';
 
 config();
 
 const app = express();
-const port = 3000;
 
 app.use(json());
 app.use('/api', routes)
 
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on http://localhost:${process.env.PORT}`);
 });
 const startApp = async () => {
   try {
